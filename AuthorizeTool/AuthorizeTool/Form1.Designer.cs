61a287b22b9237ac906c06fcd700508a403e6c8c
pangiot <1742211396@qq.com>
2018-07-02 14:34:29 +0800

    授权
    完整

---
+++ AuthorizeTool/AuthorizeTool/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace AuthorizeTool
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAuthorize = new System.Windows.Forms.Label();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAuthorize
+            // 
+            this.lblAuthorize.AutoSize = true;
+            this.lblAuthorize.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblAuthorize.Location = new System.Drawing.Point(39, 28);
+            this.lblAuthorize.Name = "lblAuthorize";
+            this.lblAuthorize.Size = new System.Drawing.Size(50, 17);
+            this.lblAuthorize.TabIndex = 0;
+            this.lblAuthorize.Text = "000000";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnGet.Location = new System.Drawing.Point(134, 21);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(94, 31);
+            this.btnGet.TabIndex = 1;
+            this.btnGet.Text = "获取授权码";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 87);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.lblAuthorize);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.ShowIcon = false;
+            this.Text = "授权码获取工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAuthorize;
+        private System.Windows.Forms.Button btnGet;
+    }
+}
+
---
+++ AuthorizeTool/AuthorizeTool/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AuthorizeTool
+{
+    public partial class FrmMain : Form
+    {
+
+
+        string url = "http://118.24.14.231:22222/iot/company/authorize?name=langchuang";
+        string result = "";
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+
+            result = Get(url);
+            this.lblAuthorize.Text = result;
+        }
+
+
+
+        /// <summary>
+        /// 指定Url地址使用Get 方式获取全部字符串
+        /// </summary>
+        /// <param name="url">请求链接地址</param>
+        /// <returns></returns>
+        public static string Get(string url)
+        {
+            string result = "";
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            try
+            {
+                //获取内容
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+            finally
+            {
+                stream.Close();
+            }
+            return result;
+
+        }
+    }
+    }
